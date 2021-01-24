13b863517864a6cdd734cdc7e3a809215acd88cc
Lucas Darrieux <lukas.darrieux@gmail.com>
2021-01-24 02:21:35 -0300

    Criação da classe de acesso ao Banco de Dados

--- app/database/Database.php
+++ app/database/Database.php
@@ -1,1 +1,26 @@
 <?php
+
+  namespace App\database;
+
+  use PDOException;
+
+  class Database {
+
+    private const HOST = "localhost";
+    private const USER = "root";
+    private const PASSWORD = "";
+    private const DATABASE = "carStore"; 
+
+    public function __construct(){
+      $this->setConnection();
+    }
+
+    private function setConnection(){
+      try {
+        $this->connection = new PDO('mysql:host='.self::HOST.';dbname='.self::DATABASE,self::USER,self::PASSWORD);
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+      } catch(PDOException $e) {
+        die('ERROR: '.$e->getMessage());
+      }
+    }
+  }
